695bd10bef56136b318ec10005d97dfd023bc231
mlemat2022 <mlemat2022@46SE49-C45PYJ2.ad.campus-eni.fr>
2023-01-10 11:27:11 +0100

    test pour les gars

---
+++ src/main/java/fr/eni/echeres/bll/CodesResultatBLL.java
@@ -0,0 +1,16 @@
+///**
+// * 
+// */
+//package fr.eni.echeres.bll;
+//
+//public abstract class CodesResultatBLL {
+//	/**
+//	 * Echec quand le nom de l'article ne respecte pas les régles définies.
+//	 */
+//	public static final int REGLE_ARTICLE_NOM_ERREUR=20000;
+//	
+//	/**
+//	 * Echec quand le nom de la liste d'articles ne respecte pas les règles définies.
+//	 */
+//	public static final int REGLE_LISTE_NOM_ERREUR=20001;
+//}
