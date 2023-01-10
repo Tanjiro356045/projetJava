0a09a3310478b776e0f62dbf263fe506d443e128
falbricias <f.albricias@laposte.net>
2023-01-10 11:47:12 +0100

    Creation package BO

---
+++ src/main/java/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,13 @@
+package fr.eni.encheres.bo;
+
+import java.io.Serializable;
+
+public class ArticleVendu implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5546842911769078680L;
+	
+
+}
