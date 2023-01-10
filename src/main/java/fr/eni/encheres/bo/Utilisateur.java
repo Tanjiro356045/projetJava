6ead18b362291c3c97cc39f41b4588a3f124927e
bguillo2022 <bguillo2022@46SE48-C4TNYJ2.ad.campus-eni.fr>
2023-01-10 15:09:12 +0100

    Merge branch 'master' of https://github.com/Tanjiro356045/projetJava
    BO utilisateurs

---
+++ src/main/java/fr/eni/encheres/bo/Utilisateur.java
@@ -0,0 +1,295 @@
+/**
+ * 
+ */
+package fr.eni.encheres.bo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bguillo2022
+ *
+ */
+public class Utilisateur implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4374948417968605957L;
+
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String noTelephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
+	private boolean administrateur;
+	private List<ArticleVendu> articleVendu = new ArrayList<>();
+	private List<Enchere> enchere = new ArrayList<>();
+
+	/**
+	 * constructeur
+	 * 
+	 */
+	public Utilisateur() {
+		super();
+	}
+
+	/**
+	 * constructeur
+	 * 
+	 * @param noUtilisateur
+	 * @param pseudo
+	 * @param nom
+	 * @param prenom
+	 * @param email
+	 * @param noTelephone
+	 * @param rue
+	 * @param codePostal
+	 * @param ville
+	 * @param motDePasse
+	 * @param credit
+	 * @param administrateur
+	 * @param articleVendu
+	 * @param enchere
+	 */
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String rue,
+			String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
+		this();
+		this.setNoUtilisateur(noUtilisateur);
+		this.setPseudo(pseudo);
+		this.setNom(nom);
+		this.setPrenom(prenom);
+		this.setEmail(email);
+		this.setRue(rue);
+		this.setCodePostal(codePostal);
+		this.setVille(ville);
+		this.setMotDePasse(motDePasse);
+		this.setCredit(credit);
+		this.setAdministrateur(administrateur);
+
+	}
+
+	/**
+	 * constructeur
+	 * 
+	 * @param noTelephone
+	 */
+	public Utilisateur(String noTelephone) {
+		this();
+		this.noTelephone = noTelephone;
+	}
+
+	/**
+	 * @return the noUtilisateur
+	 */
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	/**
+	 * @param noUtilisateur the noUtilisateur to set
+	 */
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	/**
+	 * @return the pseudo
+	 */
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	/**
+	 * @param pseudo the pseudo to set
+	 */
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the noTelephone
+	 */
+	public String getNoTelephone() {
+		return noTelephone;
+	}
+
+	/**
+	 * @param noTelephone the noTelephone to set
+	 */
+	public void setNoTelephone(String noTelephone) {
+		this.noTelephone = noTelephone;
+	}
+
+	/**
+	 * @return the rue
+	 */
+	public String getRue() {
+		return rue;
+	}
+
+	/**
+	 * @param rue the rue to set
+	 */
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	/**
+	 * @return the codePostal
+	 */
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	/**
+	 * @param codePostal the codePostal to set
+	 */
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+
+	/**
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	/**
+	 * @return the motDePasse
+	 */
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	/**
+	 * @param motDePasse the motDePasse to set
+	 */
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	/**
+	 * @return the credit
+	 */
+	public int getCredit() {
+		return credit;
+	}
+
+	/**
+	 * @param credit the credit to set
+	 */
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	/**
+	 * @return the administrateur
+	 */
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+
+	/**
+	 * @param administrateur the administrateur to set
+	 */
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+
+	/**
+	 * @return the articleVendu
+	 */
+	public List<ArticleVendu> getArticleVendu() {
+		return articleVendu;
+	}
+
+	/**
+	 * @param articleVendu the articleVendu to set
+	 */
+	public void setArticleVendu(List<ArticleVendu> articleVendu) {
+		this.articleVendu = articleVendu;
+	}
+
+	/**
+	 * @return the enchere
+	 */
+	public List<Enchere> getEnchere() {
+		return enchere;
+	}
+
+	/**
+	 * @param enchere the enchere to set
+	 */
+	public void setEnchere(List<Enchere> enchere) {
+		this.enchere = enchere;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [noUtilisateur=" + noUtilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom="
+				+ prenom + ", email=" + email + ", noTelephone=" + noTelephone + ", rue=" + rue + ", codePostal="
+				+ codePostal + ", ville=" + ville + ", motDePasse=" + motDePasse + ", credit=" + credit
+				+ ", administrateur=" + administrateur + ", articleVendu=" + articleVendu + "]";
+	}
+
+	
+	
+}
