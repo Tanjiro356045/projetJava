eaa92c462360d0963b273447f078464d40d14a51
bguillo2022 <bguillo2022@46SE48-C4TNYJ2.ad.campus-eni.fr>
2023-01-11 12:10:58 +0100

    DAOFactory

--- src/main/java/fr/eni/encheres/dao/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dao/DAOFactory.java
@@ -3,12 +3,20 @@
  */
 package fr.eni.encheres.dao;
 
+
+
 /**
  * Classe en charge de 
  * @author bguillo2022
  * @date 11 janv. 2023 - 11:57:13
  * @version projet_javaEE - v0.1
  */
-public class DAOFactory {
-
+public abstract class DAOFactory {
+	
+	public static CategorieDAO getCategorieDAO(){
+		
+		return new CategorieDAOJdbcImpl();
+	}
+	
+	
 }
