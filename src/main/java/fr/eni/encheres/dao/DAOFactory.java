c540af1b76a61ada3f694de6cd609d8d88b761e6
bguillo2022 <bguillo2022@46SE48-C4TNYJ2.ad.campus-eni.fr>
2023-01-11 12:09:25 +0100

    Début DAOFactory

---
+++ src/main/java/fr/eni/encheres/dao/DAOFactory.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.eni.encheres.dao;
+
+/**
+ * Classe en charge de 
+ * @author bguillo2022
+ * @date 11 janv. 2023 - 11:57:13
+ * @version projet_javaEE - v0.1
+ */
+public class DAOFactory {
+
+}
