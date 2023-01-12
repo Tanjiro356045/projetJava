eb0a3efa4191c5f5d74d3fd004b5335e6b00b481
mlemat2022 <mlemat2022@46SE49-C45PYJ2.ad.campus-eni.fr>
2023-01-12 13:47:50 +0100

    Servlet Utilisateur V1

--- src/main/java/fr/eni/encheres/servlet/ServletUtilisateur.java
+++ src/main/java/fr/eni/encheres/servlet/ServletUtilisateur.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
  * Servlet implementation class ServletUtilisateur
  */
 @WebServlet("/createLogin")
+
 public class ServletUtilisateur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
