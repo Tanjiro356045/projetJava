0fd5bac5437b703c393df48277d0a4bae00f40bb
mlemat2022 <mlemat2022@46SE49-C45PYJ2.ad.campus-eni.fr>
2023-01-16 10:07:10 +0100

    Mis a jour JSP ( homeCO x DECO )

---
+++ src/main/java/fr/eni/encheres/servlet/ServletAccueilDeconnecter.java
@@ -0,0 +1,35 @@
+package fr.eni.encheres.servlet;
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/AccueilDeconnecter")
+public class ServletAccueilDeconnecter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletAccueilDeconnecter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.removeAttribute("UtilisateurConnecter");
+		session.invalidate();
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accueilDeconnecter.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
